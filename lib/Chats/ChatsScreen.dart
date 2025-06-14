55d579c2dffb0d0075fbfc8c27d9548f0e448d1b
Khayooo <mabdullahkhayoo3@gamil.com>
2025-06-14 13:10:47 +0500

    Load user data from Firebase and add "My Adds" option

--- lib/Chats/ChatsScreen.dart
+++ lib/Chats/ChatsScreen.dart
@@ -1,9 +1,8 @@
-import 'package:flutter/cupertino.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-
-import '../AccountScreen.dart';
-import '../AddItemScreen.dart';
-import '../DonationItems.dart';
+import 'ChatList.dart';
+import 'NoMessageWidget.dart';
 
 class ChatsScreen extends StatefulWidget {
   const ChatsScreen({super.key});
@@ -12,16 +11,17 @@ class ChatsScreen extends StatefulWidget {
   State createState() => _ChatScreenState();
 }
 
-
 class _ChatScreenState extends State<ChatsScreen>
     with SingleTickerProviderStateMixin {
   late TabController _tabController;
+  final currentUserId = FirebaseAuth.instance.currentUser!.uid;
 
   @override
   void initState() {
     super.initState();
-    _tabController = TabController(length: 3, vsync: this);
+    _tabController = TabController(length: 2, vsync: this);
   }
+
 
   @override
   Widget build(BuildContext context) {
@@ -40,14 +40,6 @@ class _ChatScreenState extends State<ChatsScreen>
         ),
         bottom: TabBar(
           controller: _tabController,
-          labelColor: Colors.deepPurple,
-          unselectedLabelColor: Colors.grey,
-          indicatorColor: Colors.deepPurple,
-          indicatorWeight: 3,
-          labelStyle: const TextStyle(
-            fontWeight: FontWeight.w600,
-            fontSize: 14,
-          ),
           tabs: const [
             Tab(text: "Sellers"),
             Tab(text: "Donators"),
@@ -56,9 +48,8 @@ class _ChatScreenState extends State<ChatsScreen>
       ),
       body: TabBarView(
         controller: _tabController,
-        children: const [
-          NoMessageWidget(),
-          NoMessageWidget(),
+        children: [
+          ChatList(roleFilter: 'seller', currentUserId: currentUserId),
           NoMessageWidget(),
         ],
       ),
@@ -66,63 +57,3 @@ class _ChatScreenState extends State<ChatsScreen>
   }
 }
 
-class NoMessageWidget extends StatelessWidget {
-  const NoMessageWidget({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Icon(
-              Icons.message,
-              size: 100,
-              color: Colors.grey.shade400,
-            ),
-            const SizedBox(height: 20),
-            const Text(
-              "No Messages Yet?",
-              style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
-                color: Colors.deepPurple,
-              ),
-            ),
-            const SizedBox(height: 10),
-            Text(
-              "Find something you like and start a conversation!",
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                fontSize: 16,
-                color: Colors.grey.shade600,
-              ),
-            ),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.deepPurple,
-                padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 12),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(20),
-                ),
-              ),
-              onPressed: () {
-                // Navigate to home screen
-              },
-              child: const Text(
-                "Browse Items",
-                style: TextStyle(
-                  color: Colors.white,
-                  fontWeight: FontWeight.w600,
-                ),
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
