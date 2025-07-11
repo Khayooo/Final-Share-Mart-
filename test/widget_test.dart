0ccb6b781b6f5cbb407a1e4a46bbd1902ff4963b
Khayooo <mabdullahkhayoo3@gmail.com>
2025-07-11 21:33:18 +0500

    Refactor: Update import path in widget test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,8 +7,9 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:fypnewproject/Screens/main.dart';
 
-import 'package:fypnewproject/main.dart';
+
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
