76d7f7dcedde7c9f5de49a01b897c7b90c75f3e3
Khayooo <mabdullahkhayoo3@gmail.com>
2025-08-07 20:40:25 +0500

    feat: Create Reviews screen

---
+++ lib/Screens/User/Account/Reviews.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Reviews extends StatelessWidget {
+  const Reviews({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          "Reviews",
+          style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
+        ),
+      ),
+    );
+  }
+}
