1516383ca595ba095f04483a32ab80f0ca6bf523
Khayooo <mabdullahkhayoo3@gmail.com>
2025-08-07 20:37:00 +0500

    feat: Implement Product Request and Exchange Features

---
+++ lib/Model/ItemModel.dart
@@ -0,0 +1,48 @@
+
+class ItemModel {
+  final String image;
+  final String itemType;
+  final String productName;
+  final String productDescription;
+  final String productPrice;
+  final int timestamp;
+  final String uid;
+  final String userId;
+
+  ItemModel({
+    required this.image,
+    required this.itemType,
+    required this.productName,
+    required this.productDescription,
+    required this.productPrice,
+    required this.timestamp,
+    required this.uid,
+    required this.userId,
+  });
+
+  factory ItemModel.fromMap(Map<String, dynamic> map) {
+    return ItemModel(
+      image: map['image'] ?? '',
+      itemType: map['itemType'] ?? '',
+      productName: map['productName'] ?? '',
+      productDescription: map['productDescription'] ?? '',
+      productPrice: map['productPrice'] ?? '',
+      timestamp: map['timestamp'] ?? 0,
+      uid: map['uid'] ?? '',
+      userId: map['userId'] ?? '',
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'image': image,
+      'itemType': itemType,
+      'productName': productName,
+      'productDescription': productDescription,
+      'productPrice': productPrice,
+      'timestamp': timestamp,
+      'uid': uid,
+      'userId': userId,
+    };
+  }
+}
