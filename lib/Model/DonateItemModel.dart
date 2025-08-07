f1bb7980e51cb0127037265f4c6454fb8222397a
Khayooo <mabdullahkhayoo3@gmail.com>
2025-08-07 20:33:33 +0500

    Create DonateItemModel.dart

---
+++ lib/Model/DonateItemModel.dart
@@ -0,0 +1,47 @@
+class DonateItemModel {
+  final String image;
+  final String itemType;
+  final String productName;
+  final String productDescription;
+  final String productPrice;
+  final int timestamp;
+  final String uid;
+  final String userId;
+
+  DonateItemModel({
+    required this.image,
+    required this.itemType,
+    required this.productName,
+    required this.productDescription,
+    required this.productPrice,
+    required this.timestamp,
+    required this.uid,
+    required this.userId,
+  });
+
+  factory DonateItemModel.fromMap(Map<String, dynamic> map) {
+    return DonateItemModel(
+      image: map['image'] ?? '',
+      itemType: map['itemType'] ?? '',
+      productName: map['productName'] ?? '',
+      productDescription: map['productDescription'] ?? '',
+      productPrice: map['productPrice'] ?? '',
+      timestamp: map['timestamp'] ?? 0,
+      uid: map['uid'] ?? '',
+      userId: map['userId'] ?? '',
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'image': image,
+      'itemType': itemType,
+      'productName': productName,
+      'productDescription': productDescription,
+      'productPrice': productPrice,
+      'timestamp': timestamp,
+      'uid': uid,
+      'userId': userId,
+    };
+  }
+}
