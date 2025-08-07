a565b3bc5cc95865453c3e1742f753a568214f07
Khayooo <mabdullahkhayoo3@gmail.com>
2025-08-07 20:23:21 +0500

    update the file

--- lib/Screens/main.dart
+++ lib/Screens/main.dart
@@ -1,9 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import '../add product features/Product_Request.dart';
-import '../add product features/exchange_product.dart';
-import 'HomePage.dart';
+import 'User/HomePage.dart';
 import 'SplashScreen.dart';
 import 'firebase_options.dart';
 
