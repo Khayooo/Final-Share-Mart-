6af3edd7bfd0f200f0a8eda59f03c84d25f5203a
Khayooo <mabdullahkhayoo3@gmail.com>
2025-08-11 21:21:47 +0500

    Create ReportsScreen.dart

---
+++ lib/Screens/Admin/ReportsScreen.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class ReportsScreen extends StatelessWidget {
+  const ReportsScreen({super.key});
+  @override
+  Widget build(BuildContext context) => Scaffold(appBar: AppBar(title: const Text("Reports")));
+}
