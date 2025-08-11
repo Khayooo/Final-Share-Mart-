2617657d3af4129f3deb8a666fc76482a1b0068c
Khayooo <mabdullahkhayoo3@gmail.com>
2025-08-11 21:19:43 +0500

    Create AdminNotificationsScreen.dart

---
+++ lib/Screens/Admin/AdminNotificationsScreen.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class AdminNotificationsScreen extends StatelessWidget {
+  const AdminNotificationsScreen({super.key});
+  @override
+  Widget build(BuildContext context) => Scaffold(appBar: AppBar(title: const Text("Settings")));
+}
