6b9f5f76c0fa26a1268c15ee2b291687cdc01b29
Khayooo <mabdullahkhayoo3@gmail.com>
2025-08-15 23:23:26 +0500

    Delete chatDetailScreen.dart

--- lib/Chats/serverkey.dart
+++
@@ -1,6 +0,0 @@
-// class GetServerKey {
-//   Future<String> server_token() async {
-//     // Replace with actual logic if needed. For now, return hardcoded key.
-//     return 'YOUR_FCM_SERVER_KEY';
-//   }
-// }
